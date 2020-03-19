15c4abb8315951682aa08d9577add1c41846de11
onerunsall <413038044@qq.com>
2020-03-19 19:19:46 +0800

    1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ App({
     userInfo: null,
     userInfoDetail: [],
     entity: [],
-    url: 'https://test.ypt.njshangka.com',
+    url: 'https://ypt.njshangka.com',
     version: '2003160917-8bdaf1f',
     versionIntro: '修复了部分BUG\n优化了部分体验',
     cookie: '',
