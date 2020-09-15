44d35a1d3bdf2107994dfd18069b63fe6628c707
xuxk13046567620 <839362863@qq.com>
2020-09-15 15:36:52 +0800

    xuxk2009151536

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ App({
     userInfoDetail: [],
     entity: [],
     url: 'https://ypt.njshangka.com',
-    version: '20.0601.927',
+    version: '20.0915.1536',
     versionIntro: '修复了部分BUG\n优化了部分体验',
     cookie: '',
     withoutLogin:'',
